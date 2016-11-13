f052a2321032c5a6d93e2d3901e950f6001a89d7
Niharika Rastogi <dev.NiharikaRastogi@gmail.com>
2016-11-13 06:25:36 +0530

    Add sample widgets

---
+++ app/src/main/java/in/edu/galgotiasuniversity/widget/WidgetProvider.java
@@ -0,0 +1,50 @@
+package in.edu.galgotiasuniversity.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.RemoteViews;
+
+import com.sam_chordas.android.stockhawk.R;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class WidgetProvider extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        Intent svcIntent = new Intent(context, WidgetService.class);
+        svcIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        svcIntent.setData(Uri.parse(svcIntent.toUri(Intent.URI_INTENT_SCHEME)));
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.app_widget);
+        views.setEmptyView(R.id.widget_list_view, R.id.emptyView);
+        views.setRemoteAdapter(R.id.widget_list_view, svcIntent);
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+        appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetId, R.id.widget_list_view);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
---
+++ app/src/main/java/in/edu/galgotiasuniversity/widget/WidgetViewsFactory.java
@@ -0,0 +1,95 @@
+package in.edu.galgotiasuniversity.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.data.QuoteColumns;
+import com.sam_chordas.android.stockhawk.data.QuoteProvider;
+
+/**
+ * Created by Rohan Garg on 15-06-2016.
+ */
+public class WidgetViewsFactory implements RemoteViewsService.RemoteViewsFactory {
+
+    Context mContext;
+    Cursor mCursor;
+    int mWidgetId;
+
+    WidgetViewsFactory(Context context, Intent intent) {
+        mContext = context;
+        mWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+    }
+
+    @Override
+    public void onCreate() {
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mCursor != null)
+            mCursor.close();
+    }
+
+    @Override
+    public int getCount() {
+        if (mCursor != null)
+            return mCursor.getCount();
+        return 0;
+    }
+
+    @Override
+    public RemoteViews getViewAt(int position) {
+        if (mCursor != null) {
+            mCursor.moveToPosition(position);
+            RemoteViews view = new RemoteViews(mContext.getPackageName(), R.layout.widget_list_item_quote);
+            view.setTextViewText(R.id.stock_symbol, mCursor.getString(mCursor.getColumnIndex(QuoteColumns.SYMBOL)));
+            view.setTextViewText(R.id.bid_price, mCursor.getString(mCursor.getColumnIndex(QuoteColumns.BIDPRICE)));
+            view.setTextViewText(R.id.change, mCursor.getString(mCursor.getColumnIndex(QuoteColumns.CHANGE)));
+
+            if (mCursor.getInt(mCursor.getColumnIndex(QuoteColumns.ISUP)) == 1) {
+                view.setInt(R.id.change, "setBackgroundResource", R.drawable.percent_change_pill_green);
+            } else {
+                view.setInt(R.id.change, "setBackgroundResource", R.drawable.percent_change_pill_red);
+            }
+            return view;
+        }
+        return null;
+    }
+
+    @Override
+    public void onDataSetChanged() {
+        if (mCursor != null) {
+            mCursor.close();
+        }
+        String[] projection = new String[]{QuoteColumns._ID, QuoteColumns.SYMBOL, QuoteColumns.BIDPRICE,
+                QuoteColumns.PERCENT_CHANGE, QuoteColumns.CHANGE, QuoteColumns.ISUP};
+        String selection = QuoteColumns.ISCURRENT + " = ?";
+        String[] selectionArgs = new String[]{"1"};
+        mCursor = mContext.getContentResolver().query(QuoteProvider.Quotes.CONTENT_URI, projection, selection, selectionArgs, null);
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+}
